f654f64222308f134814fbecb379e9b9dab7cca0
khuranamanan <112554344+khuranamanan@users.noreply.github.com>
2023-07-13 21:53:33 +0530

    Update EventDetailsPage.jsx

--- src/pages/EventDetailsPage.jsx
+++ src/pages/EventDetailsPage.jsx
@@ -98,7 +98,7 @@ export default function EventDetailsPage() {
           </li>
         ))}
       </ul>
-      <div className="mx-4">
+      <div className="my-4">
         <RsvpModal event={findCurrEvent} />
       </div>
     </div>
